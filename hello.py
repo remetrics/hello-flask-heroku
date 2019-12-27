26e58d3436c5866f8e538050927e6b3312cdc792
Jens Bonerz <57752745+remetrics@users.noreply.github.com>
2019-12-27 21:14:46 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,23 +1,15 @@
-from flask import Flask, request
-import json
-import pprint
+import socket
 
-class LoggingMiddleware(object):
-    def __init__(self, app):
-        self._app = app
-
-    def __call__(self, environ, resp):
-        errorlog = environ['wsgi.errors']
-        pprint.pprint(('REQUEST', environ), stream=errorlog)
-
-        def log_response(status, headers, *args):
-            pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
-            return resp(status, headers, *args)
-
-        return self._app(environ, log_response)
-
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.wsgi_app = LoggingMiddleware(app.wsgi_app)
-    app.run()
+host = ''
+port = 80
+backlog = 5
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(backlog)
+while 1:
+    client, address = s.accept()
+    data = client.recv(size)
+    if data:
+        client.send(data)
+    client.close()
