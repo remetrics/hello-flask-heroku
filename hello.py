90219db9b2bbcd5b01f36abf13808cf78ccf6365
Jens Bonerz <57752745+remetrics@users.noreply.github.com>
2019-12-27 22:03:41 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def forward_request():
-    return repr(os.environ)
+    return repr(os.environ) + repr(request.access_route)
 
 if __name__ == '__main__':
     app.run()
