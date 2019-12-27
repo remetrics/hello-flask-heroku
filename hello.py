140987e6b5b3a71494f85d8453f50bafe32750c3
Jens Bonerz <57752745+remetrics@users.noreply.github.com>
2019-12-27 21:33:08 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,10 @@
 from flask import Flask, request
 import json
-import os
-
 app = Flask(__name__)
 
 @app.route('/')
 def forward_request():
     return json.dumps(list(os.environ.keys()))
-
-app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run()
