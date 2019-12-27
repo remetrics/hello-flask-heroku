5e9af24fe65b383819f49ef3d6786e9a40e9d837
Jens Bonerz <57752745+remetrics@users.noreply.github.com>
2019-12-27 21:12:47 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,23 @@
 from flask import Flask, request
 import json
+import pprint
+
+class LoggingMiddleware(object):
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, environ, resp):
+        errorlog = environ['wsgi.errors']
+        pprint.pprint(('REQUEST', environ), stream=errorlog)
+
+        def log_response(status, headers, *args):
+            pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
+            return resp(status, headers, *args)
+
+        return self._app(environ, log_response)
 
 app = Flask(__name__)
 
-@app.route('/')
-def forward_request():
-    request.get_data()
-    return request.data
-
 if __name__ == '__main__':
+    app.wsgi_app = LoggingMiddleware(app.wsgi_app)
     app.run()
