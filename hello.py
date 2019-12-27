255868467b86665d1b3d9e41ddec09ca8a81535c
Jens Bonerz <57752745+remetrics@users.noreply.github.com>
2019-12-27 09:27:09 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def forward_request():
     #test
-    json.dumps(dict(request.headers))
+    return json.dumps(dict(request.headers))
 
 if __name__ == '__main__':
     app.run()
